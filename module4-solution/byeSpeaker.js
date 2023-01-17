d9a2e0c3c302b21ed77be6e1b5e893bbdf069b5d
thienantran <46636456+thienantran@users.noreply.github.com>
2023-01-16 19:32:09 -0800

    Create byeSpeaker.js

---
+++ module4-solution/byeSpeaker.js
@@ -0,0 +1,8 @@
+(function () {
+  var byeSpeaker = {};
+  var speakWord = 'Good Bye';
+  byeSpeaker.speak = function (name) {
+    console.log(speakWord + ' ' + name);
+  };
+  global.byeSpeaker = byeSpeaker;
+})();
